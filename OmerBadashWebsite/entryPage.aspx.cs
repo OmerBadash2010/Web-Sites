fb80f91862057c49b83252bc522066f99f5d046f
User <User@PC302-17>
2026-04-14 16:17:43 +0300

    Session

--- OmerBadashWebsite/entryPage.aspx.cs
+++ OmerBadashWebsite/entryPage.aspx.cs
@@ -1,42 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 
 public partial class entryPage : System.Web.UI.Page
 {
     public string st = "";
+
     protected void Page_Load(object sender, EventArgs e)
     {
         if (Page.IsPostBack)
         {
-
             string email = Request.Form["email"];
             string password = Request.Form["password"];
 
-            if ((email == "N@gmail.com") && (password == "1"))
+            // Admin check
+            if (email == "N@gmail.com" && password == "1")
+            {
+                Session["userName"] = "Admin";
                 Response.Redirect("managementPage.aspx");
+            }
             else
             {
                 string sqlSelect = "SELECT * FROM tUsers WHERE email = N'"
                                     + email + "' AND password = N'"
                                     + password + "'";
 
-
                 bool isExist = MyAdoHelper.IsExist(sqlSelect);
 
                 if (isExist)
-                    //st = "רשום";
+                {
+                    Session["userName"] = "User";
                     Response.Redirect("HomePage.aspx");
+                }
                 else
-                    //st = "אורח";
+                {
+                    Session["userName"] = "Guest";
                     st = "אינך רשום";
-
-
+                }
             }
-
         }
     }
 }
