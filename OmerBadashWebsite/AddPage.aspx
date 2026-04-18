efa549054dc39b8b9bdd03103ceb7724b86ca063
Omer <Omer@LAPTOP-IH8G08GK>
2026-04-18 19:45:25 +0300

    סיימתי עבודת פסח רק צריך עזקרה

---
+++ OmerBadashWebsite/AddPage.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="AddPage.aspx.cs" Inherits="AddPage" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder2" Runat="Server">
+
+    <h2>ריאל מדריד</h2>
+
+    גיל: <input type="number" name="Age" id="Age" />
+    <br />
+    תאריך הצטרפות: <input type="text" name="JoinDate" id="JoinDate" />
+    <br />
+    לאום: <input type="text" name="Nationality" id="Nationality" />
+    <br />
+    גובה: <input type="number" name="Height" id="Height" />
+    <br />
+    עמדה: <input type="text" name="Position" id="Position" />
+    <br />
+    מספר חולצה: <input type="number" name="ShirtNumber" id="ShirtNumber" />
+    <br />
+    רגל חזקה: <input type="text" name="StrongFoot" id="StrongFoot" />
+    <br />
+
+    <input type="submit" value="שלח" />
+
+</asp:Content>
---
+++ OmerBadashWebsite/AddPage.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public partial class AddPage : System.Web.UI.Page
+{
+    public string Age;
+    public string JoinDate;
+    public string Nationality;
+    public string Height;
+    public string Position;
+    public string ShirtNumber;
+    public string StrongFoot;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (IsPostBack)
+        {
+            Age = Request.Form["Age"];
+            JoinDate = Request.Form["JoinDate"];
+            Nationality = Request.Form["Nationality"];
+            Height = Request.Form["Height"];
+            Position = Request.Form["Position"];
+            ShirtNumber = Request.Form["ShirtNumber"];
+            StrongFoot = Request.Form["StrongFoot"];
+
+            string sqlInsert =
+                "INSERT INTO morielBall " +
+                "(Age, JoinDate, Nationality, Height, Position, ShirtNumber, StrongFoot) VALUES (" +
+                Age + "," +
+                "'" + JoinDate + "'," +
+                "N'" + Nationality + "'," +
+                Height + "," +
+                "N'" + Position + "'," +
+                ShirtNumber + "," +
+                "N'" + StrongFoot + "'" +
+                ")";
+
+            MyAdoHelper.DoQuery(sqlInsert, "yourConnectionString");
+        }
+    }
+}
